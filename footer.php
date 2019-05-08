ef969cd08699040a63f81a7f080d9e3f16478d03
fxrl <f.rossel@gmx.de>
2019-05-08 19:16:35 +0200

    basic styling footer and index

--- footer.php
+++ footer.php
@@ -1,4 +1,35 @@
-<footer> 
+<footer>
+  <div class="container-fluid">
+    <!-- social logos -->
+    <div class="row justify-content-between">
+      <div class="col-xs-3">
+        <img src='https://via.placeholder.com/50' alt='social'>
+      </div>
+      <div class="col-xs-3">
+        <img src='https://via.placeholder.com/50' alt='social'>
+      </div>
+      <div class="col-xs-3">
+        <img src='https://via.placeholder.com/50' alt='social'>
+      </div>
+      <div class="col-xs-3">
+        <img src='https://via.placeholder.com/50' alt='social'>
+      </div>
+    </div>
+    <!-- map -->
+    <div class="row">
+      <div class="col footer__map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2121.5290411175056!2d13.326172180567168!3d57.87719228126663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465a90c7630eff03%3A0xe2c9425c1cc0403d!2sKnalten+Ekologiska+Gr%C3%B6nsaker!5e0!3m2!1sen!2sus!4v1557335328332!5m2!1sen!2sus" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>      </div>
+    </div>
+    <!-- links -->
+    <div class="row">
+      <div class="col footer__menu">
+        <a href="#">Kontakt</a>
+        <a href="#">Kontakt</a>
+        <a href="#">Kontakt</a>
+        <a href="#">Kontakt</a>
+      </div>
+    </div>
+  </div>
 
 </footer>
     <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
 <!-- hero-section -->
-<div class="container-fluid">
+<div class="container-fluid hero">
     <div class="row">
       <div class="col-12">
         <img src='https://via.placeholder.com/1000x700'>
@@ -10,7 +10,7 @@
 </div>
 
 <!-- welcome section -->
-<div class="container">
+<div class="container welcome">
   <div class="row">
     <div class="col-md-6">
       <h1>Knalten</h1>
@@ -18,38 +18,47 @@
 		    men vi gör det mesta av arbetet manuellt. För att underlätta detta arbete använder vi ett magiskt redskap: Bredgrepen.
 		  <p>
       <button class='btn btn-primary'>Hitta Hit</button>
-
     </div>
   </div>
-
+  <div class="row welcome__certificates">
+    <div class="col-xs-6">
+        <img src='https://via.placeholder.com/50' alt='cert'>
+    </div>
+    <div class="col-xs-6">
+        <img src='https://via.placeholder.com/50' alt='cert'>
+    </div>
+  </div>
 </div>
 
 <!-- latest news section -->
-<div class="container">
-<div class="row">
-    <div class="col"> 
-      <?php if ( have_posts() ) : while ( have_posts() ) :   the_post(); ?>
-        <div class="card mb-3">
-          <img class="card-img-top" src="https://via.placeholder.com/100" alt="Card image cap">
-          <div class="card-body">
-          <h5 class="card-title">		
-            <a href="<?php the_permalink() ?>">
-			        <?php the_title(); ?>
-            </a>
-          </h5>
-          <p class="card-text">	<?php the_content(); ?></p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-	<?php endwhile; else: ?>
-	<p>There no posts to show</p>
-	<?php endif; ?>
-</div>
-
+<section class='latest-news-wrapper'>
+  <div class="container latest-news">
+    <div class="row">
+        <div class="col-12">
+          <h2>Latest News</h2>
+        </div>
+          <!-- the loop -->
+          <?php if ( have_posts() ) : while ( have_posts() ) :   the_post(); ?>
+          <div class="col-md-4">
+            <div class="card">
+              <img class="card-img-top" src="https://via.placeholder.com/100" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">		
+                    <?php the_title(); ?>
+                </h5>
+                <p class="card-text">	<?php the_content(); ?></p>
+                <a href="<?php the_permalink() ?>">Read More</a>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+      <?php endwhile; else: ?>
+      <p>There no posts to show</p>
+      <?php endif; ?>
     </div>
   </div>
+</section>
 
-</div>
 
 
 
