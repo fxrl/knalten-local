4831fa776a2bbd1fd36daf5c0e04a3a8458f089c
Sonia <soniamianji1@gmail.com>
2019-05-09 14:09:05 +0200

    git ignore modified

--- vegetables.php
+++ vegetables.php
@@ -70,22 +70,20 @@
 <div class="container welcome">
 	<div class="row">
 		<div class="col-md-6">
-			<h1> GRÖNSAKER</h1>
-			<h3> Från trädgården till tallriken! Vi skördar på morgonen, ni har dem på tallriken på kvällen!</h3>
-			<p>I våra 14 tegar odlar vi ett sortiment av ca 40 olika ekologiska grönsaker. Våra hästar är ett stort stöd i förberedelsearbetet,
-				men vi gör det mesta av arbetet manuellt. För att underlätta detta arbete använder vi ett magiskt redskap: Bredgrepen.
-				<p>
-					<button class='btn btn-primary'>Försäljningställen</button>
+
+			<h3> Vårt motto är närodlat, nyskördat och smakrikt!</h3>
+			<p>Grönsaksodlingarna är både ekologiska- och KRAV-certifierade (HS Certifiering AB, SE-EKO-04) och omfattar ca en halv hektar
+				mark.
+			</p>
+			<p>Under säsongen har du möjlighet att njuta av våra nyskördade, näringsrika och mycket smakfulla grönsaker varje vecka.
+			</p>
+			<p>Precis som i Frankrike där grönsakshandlaren alltid gav Sonias mormor det bästa han hade för dagen, kan ni alltid lita
+				på att vi på Knalten ger er det bästa vi har för dagen och säsong.
+			</p>
+
 		</div>
 	</div>
-	<div class="row welcome__certificates">
-		<div class="col-xs-6">
-			<img src='https://via.placeholder.com/50' alt='cert'>
-		</div>
-		<div class="col-xs-6">
-			<img src='https://via.placeholder.com/50' alt='cert'>
-		</div>
-	</div>
+
 </div>
 
 <!-- footer -->
