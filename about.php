8a6ebe29b09322b21fa7db6b8d302581473ec51e
JustynaTheBold <45666291+JustynaTheBold@users.noreply.github.com>
2019-05-11 14:32:19 +0200

    MY aout

---
+++ about.php
@@ -0,0 +1,80 @@
+//about.php
+
+
+<?php 
+/* Template Name:  About  */ 
+get_header();?>
+
+<!-- hero-section -->
+<div class="container-fluid hero">
+    <div class="row">
+      <div class="col-12">
+        <img src='https://via.placeholder.com/1000x700'>
+      </div>
+    </div>
+</div>
+
+<!--  vad gor vi pa knalten -->
+<div class='container'>
+    <div class='row'>
+        <div class='col-12'>
+            <h1>OM OSS<h1>
+            <h2>Vad gör vi på knalten ?<h2>
+            <p>
+            På Knalten odlar vi ett 40-tal olika grönsaker för den lokala marknaden och reko-ringen i Ulricehamn och Borås. Dessutom är vi direktleverantör till ett antal lokal restauranger.
+            Grönsaksodlingarna är certifierat ekologiska och KRAV-märkta. I dagsläget odlar vi på ungefär ett halv hektar. Vi plöjer inte utan odlar istället i fasta bäddar. Det gör att vi kan ha en skonsam men hög avkastning på liten yta, en lång skördesäsong och dessutom arbeta med att ständigt förbättra jorden.
+            </p>
+        </div>
+    </div>
+</div>
+
+<section class='whoarewe'>
+    
+    <!--- vem ar vi--->
+    <div class='container'>
+        <div class='row'>
+            <div class='col-12'>
+                <h2>Varför flyttade vi till Sverige?<h2>
+                <p>
+                    Vi kom till Sverige och Ulricehamn för första gången 2007 att besöka gamla vänner. När vi kom hit slogs vi direkt av hur fint det var; skogen och naturen var fantastiskt vacker! Det blev tre veckor i paradiset med underbart väder, cyklande och badande.
+                    Vi drogs till området och efter sista besöket 2011 var det mycket svårt att åka tillbaka till storstaden Lyon igen. Våra svenska vänner frågade mest på skoj; ”Men varför flyttar ni inte hit?” Det tog lite fundering, men svaret var egentligen uppenbart: ”Jo, vi flyttar hit!
+                </p>
+            </div>
+        </div>
+
+        <!--- Pierre --->
+        <div class='row'>
+            <div class='col-md-6'>
+                <img src='http://knalten-eko.se/wp-content/uploads/2018/04/Pierre5.png'>
+                <h2>Pierre<h2>
+                <p>
+                Pierre kommer ursprungligen från Lyon i Frankrike, ”La Capitale de la Gastronomie”, eller gastronomins huvudstad. Hans växte upp i en familj där mat och matlagning var ett centralt och sammanbindande tema.
+                </p>
+            </div>
+
+        <!--- Sonia --->
+        
+            <div class='col-md-6'>
+                <img src='http://knalten-eko.se/wp-content/uploads/2018/03/Sonia-Fleur-e1522690515727-300x201.jpg'>
+                <h2>Sonia</h2>
+                <p>
+                Sonia kommer från andra sidan i Frankrike, från den vackra atlantkusten. Hennes kärlek för lokalproducerat mat och grönsaker kommer ursprungligen från hennes mormor. ”Grönsaker fanns alltid på menyn hos mormor!” Varje vecka brukade hon att gå på marknaden med mormor och välja de bästa grönsakerna efter säsong.  ” Vad har du idag för mig, Michel!” Brukade mormor fråga.
+                </p>
+            </div>
+        </div>
+
+         <!--- QUOTATION --->
+         <div class='row'>
+            <div class='col-12'>
+                <p>
+                    "För oss, liksom för mormor, är relationen och förtroendet mellan producent och kunder av yttersta vikt. Det är därför som vi tycker om lokalproducerat mat och ekologisk."
+                </p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<!-- footer -->
+<?php get_footer() ?>
