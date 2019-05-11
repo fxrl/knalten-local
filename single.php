f097850be583100d820a593294e8968f982e18d0
fxrl <f.rossel@gmx.de>
2019-05-11 18:18:25 +0200

    styling of single.php

--- single.php
+++ single.php
@@ -14,33 +14,54 @@
 
     $slides = $dynamic_featured_image->get_featured_images([get_the_id()]);
 ?>
-<div class="container">
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-        <?php 
-            foreach ($slides as $slide) {
-                $image = $slide['full'];
-                echo "
-                <div class='carousel-item'>
-                    <img class='d-block w-100' src='$image' alt='First slide'>              
-                </div>
-                ";
-            }
-        ?>
+
+<div class="container post__heading">
+    <div class="row">
+        <div class="col-12">
+            <?php
+                the_title('<h1>','</h1>');
+            ?>
+        </div>
+        <div class="col-12 post__meta">
+            <span class="badge badge-success">
+                    <?php the_date(); ?>
+                </span>
+                <span class="badge badge-success">
+                    Author:
+                    <?php  the_author(); ?>
+                </span>
+        </div>
+
     </div>
-
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-
-
-  </div>
 </div>
+
+<div class="container">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+            <?php 
+                foreach ($slides as $slide) {
+                    $image = $slide['full'];
+                    echo "
+                    <div class='carousel-item'>
+                        <img class='d-block w-100' src='$image' alt='First slide'>              
+                    </div>
+                    ";
+                }
+            ?>
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
+
+    </div>
+    </div>
 
 </div>
 
@@ -50,16 +71,8 @@
     <div class='row'>
         <div class='col-12'>
             <?php
-                the_title('<h1>','</h1>');
                 the_content();
             ?>
-            <span class="badge badge-success">
-                <?php the_date(); ?>
-            </span>
-            <span class="badge badge-success">
-                Author:
-                <?php  the_author(); ?>
-            </span>
         </div>
     </div>
 </div>
