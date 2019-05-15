9ec4c2d77f8e327f3db8daa0ecdf1dac6ad4a157
Sonia <soniamianji1@gmail.com>
2019-05-15 13:18:52 +0200

    juridisk informatin added

---
+++ juridiskinfo.php
@@ -0,0 +1,39 @@
+<?php 
+/* Template Name:  juridisk Information  */ 
+get_header();?>
+
+<div class="container introProducts juridisk">
+    <div class="row">
+        <div class="col-sm-12">
+            <h2>JURIDISK INFORMATION</h2>
+            <p>Här hittar du information som myndigheter enligt lag ska lämna vid användning av sina webbplatser.</p>
+
+            <h4> Kontaktuppgifter </h4>
+            <p>Knalten Ekologiska Grönsaker</p>
+            <p>Postadress: Salunda 109, 52399 Hökerum</p>
+            <p>Telefon: 070-302 73 03</p>
+            <p>E–postadress:<span> tradgarden@knalten-eko.se</span></p>
+            <p>Organisationsnummer: 791206-1780</p>
+            <p>Registreringsnummer för mervärdesskatt:</p>
+            <p>SE791206178001</p>
+            <p>Eko-märker: HS Certifiering AB, SE-EKO-04</p>
+            <p></p>
+            <p></p>
+
+            <h4> Användarvillkor </h4>
+            <p>Knalten tillhandahåller webbplatser (”Webbplatser”) enligt de användarvillkor som anges nedan. Om du inte godtar att följa dessa användarvillkor ska du inte använda Knaltens Webbplatser.</p>
+
+            <h4> Immateriella rättigheter </h4>
+            <p>Rättigheterna till hela innehållet på Knalten Webbplatser, såsom text, namn, bilder, ritningar, logotyper, och ikoner, tillhör Knalten. Alla rättigheter, som inte uttryckligen beviljas häri, är reserverade. Kopiering, överföring, ändring, lagring, publicering och distribution av innehållet på Knaltens Webbplatser eller delar därav är förbjudet utan Knaltens föregående skriftliga medgivande. Användningen av Knaltens Webbplatser för personliga, icke-kommersiella syften är tillåtet.</p>
+
+            <h4>Länkar</h4>
+            <p>Länkar till tredje parters sidor tillhandahålls endast för bekvämlighetens skull. Vi uttrycker ingen åsikt om innehållet på tredje parters sidor och frånsäger oss uttryckligen allt ansvar för all information från tredje part och användningen av sådan information. Alla användare samtycker till att åtkomst till och användning av webbplatsen eller av andra webbplatser som den länkar till och dessa webbplatsers innehåll sker på egen risk.</p>
+
+            <h4>Varumärken
+            </h4>
+            <p>Alla varumärken som nämns tillhör respektive ägare. Frånvaron av ett varumärke betyder inte att varumärket inte är skyddat.</p>
+        </div>
+    </div>
+</div>
+<!-- footer -->
+<?php get_footer() ?>
